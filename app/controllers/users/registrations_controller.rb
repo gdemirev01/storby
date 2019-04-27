c69c9f2526e06fbff8c0bac2e44f1441147289ec
Gdemirev01 <gdemirev78@gmail.com>
2019-04-27 22:45:30 +0300

    profile pic fucking fix...

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,7 +41,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-    resource.role = Role.find_by(name: params[:user][:role])
     resource_updated = update_resource(resource, account_update_params)
     yield resource if block_given?
     if resource_updated
